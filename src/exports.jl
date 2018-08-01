80f8e8fac8383866b7805b5ff1f4b8981422c97f
Nick Bavafa <nbavafa@gmail.com>
2018-08-01 11:16:21 -0700

    Added exports

---
+++ src/exports.jl
@@ -0,0 +1,11 @@
+
+
+export
+    dist_matrix,
+    dist_matrix_mt,
+    symmetric_dist_matrix,
+    symmetric_dist_matrix_mt,
+    smd_mf,
+    smd_distance_wrapper,
+    fst_wrapper,
+    permutation_test
